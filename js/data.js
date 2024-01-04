2937e0a10b796363947b7ab59282fb5bd650ce12
Alvin <alvinvaati@gmail.com>
2024-01-04 23:35:15 +0300

    datafile

---
+++ js/data.js
@@ -0,0 +1,28 @@
+export const dataObj = {
+    monthly: {
+        plan: {
+            arcade: '9/mo',
+            advanced: '12/mo',
+            pro: '15/mo'
+        },
+        addOn: {
+            onlineService: '1/mo',
+            cloudStorage: '2/mo',
+            profile: '2/mo'
+        },
+        discount: '0'
+    },
+    yearly: {
+        plan: {
+            arcade: '90/yr',
+            advanced: '120/yr',
+            pro: '150/yr'
+        },
+        addOn: {
+            onlineService: '10/yr',
+            cloudStorage: '20/yr',
+            profile: '20/yr'
+        },
+        discount: '2'
+    }
+}
