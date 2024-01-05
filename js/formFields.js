cdedf32ae4a2481a20b2164ef9e7ec5d74de687c
Alvin <alvinvaati@gmail.com>
2024-01-05 18:33:54 +0300

    form fields

---
+++ js/formFields.js
@@ -0,0 +1,37 @@
+//form
+export const form = document.getElementById('payment');
+//step 1
+export const nameField = document.getElementById('person-name');
+export const emailField = document.getElementById('email');
+export const phoneField = document.getElementById('phone');
+
+//step 2
+export const planField = document.getElementById('arcade');
+export const planField2 = document.getElementById('advanced');
+export const planField3 = document.getElementById('pro');
+
+export const arcadeOptionPrice = document.getElementById('arcade-option-price');//acrade plan price
+export const advancedOptionPrice = document.getElementById('advanced-option-price');//advanced plan price
+export const proOptionPrice = document.getElementById('pro-option-price');//pro plan price
+export const acradeDiscount = document.getElementById('arcade-discount');//arcade discount
+export const advancedDiscount = document.getElementById('advanced-discount');//advanced discount
+export const proDiscount = document.getElementById('pro-discount');//pro discount
+
+export const billingM = document.getElementById('monthly-billing');//monthly toggle
+export const billingY = document.getElementById('yearly-billing');//yearly toggle
+
+//step 3
+export const onlineService = document.getElementById('online-service');
+export const largeStorage = document.getElementById('large-storage');
+export const customization = document.getElementById('customization');
+export const onlineOption = document.getElementById('online-option');
+export const storageOption = document.getElementById('storage-option');
+export const customizationOption = document.getElementById('customization-option');
+
+//step 4
+export const planType = document.getElementById('plan-type');
+export const planPrice = document.getElementById('plan-price');
+export const addOnSummary = document.querySelector('.add-on-summary');
+
+export const total = document.querySelector('.total');
+
